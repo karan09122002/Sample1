63be45a51e1757478fd641bb5cd68a23e4ef1955
Karan <karanrijhwani108y@gmail.com>
2024-12-10 00:35:42 +0530

    This is first commit

---
+++ SampleDemo.java
@@ -0,0 +1,8 @@
+package now;
+
+public class SampleDemo {
+	public static void main(String args[]){
+		System.out.println("This is Demo ");
+	}
+
+}
---
+++ SampleTest.java
@@ -0,0 +1,8 @@
+package now;
+
+public class SampleTest {
+	public static void main(String args[]) {
+		System.out.println("this is test");
+	}
+
+}
