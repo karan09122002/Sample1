50cd2da70a4c8b9abcf06368521293c34672a9e8
Karan <karanrijhwani108y@gmail.com>
2024-12-10 00:48:40 +0530

    Changes in Demo.java

--- SampleDemo.java
+++ SampleDemo.java
@@ -2,7 +2,7 @@ package now;
 
 public class SampleDemo {
 	public static void main(String args[]){
-		System.out.println("This is Demo ");
+		System.out.println("This is Demo with change 1 time ");
 	}
 
 }
